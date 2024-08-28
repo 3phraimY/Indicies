ff46abddda7eb7c6893be052ba41cb28f17252ed
3phraimY <ephraim.young@cuw.edu>
2024-08-27 20:18:47 -0500

    added comments

--- main.cpp
+++ main.cpp
@@ -4,11 +4,17 @@ using namespace std;
 int main() {
     int intArray[4] = {2,7,11,15};
     int target = 9;
+
+    //determing length of intArray based on storage size
     int length = sizeof(intArray) / sizeof(intArray[0]);
+
+    //iterate through all elements in array
     for(int i = 0; i < length; i++)
     {
+        //iterate through all elements with indicies greater than i
         for(int j = i + 1; j < length; j++)
         {
+            //check if sum of elements match target
             if(intArray[i] + intArray[j] == target)
             {
                 cout <<"indicies are " << i << ", " << j << endl;
